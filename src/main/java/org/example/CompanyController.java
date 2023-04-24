79903216ad1101a84a9fb531f3dab76df5c9700c
hteleb <hanan.ebraheem@gmail.com>
2023-04-24 19:18:52 -0400

    Add datetime in utc formate

--- src/main/java/org/example/CompanyController.java
+++ src/main/java/org/example/CompanyController.java
@@ -6,13 +6,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+
 @RestController
 public class CompanyController {
 
     @GetMapping("/v1/companies/{id}")
     public ResponseEntity<Company> getCompanies(@PathVariable("id") String id) {
 
-        Company company1 = new Company("company1", "04/22/2023", "12/01/2023");
+        System.out.println(Instant.now().toString());
+        Company company1 = new Company("company1", ZonedDateTime.of(2023, 4, 22, 8, 30, 20, 100, ZoneOffset.UTC).toString(),
+                ZonedDateTime.of(2023, 12, 31, 8, 30, 20, 100, ZoneOffset.UTC).toString());
         return ResponseEntity.ok(company1);
     }
 }
