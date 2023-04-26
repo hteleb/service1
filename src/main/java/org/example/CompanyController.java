18f04c9942dbba098f7c14f56469035e02ba09b3
hteleb <hanan.ebraheem@gmail.com>
2023-04-26 13:58:43 -0400

    Fix v2 call

--- src/main/java/org/example/CompanyController.java
+++ src/main/java/org/example/CompanyController.java
@@ -1,29 +1,41 @@
 package org.example;
 
+import org.example.Entity.CompanyVersionTwo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.Instant;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
 import java.util.Map;
 
 @RestController
 public class CompanyController {
 
     @Autowired
-    private Map<String, Company> companiesMap;
+    private Map<String, Company> companiesVersionOneMap;
+
+    @Autowired
+    private Map<String, CompanyVersionTwo> companiesVersionTwoMap;
+
     @GetMapping("/v1/companies/{id}")
     public ResponseEntity<Company> getCompanies(@PathVariable("id") String id) {
 
-        if(companiesMap.containsKey(id))
-            return ResponseEntity.ok(companiesMap.get(id));
+        if(companiesVersionOneMap.containsKey(id))
+            return ResponseEntity.ok(companiesVersionOneMap.get(id));
        return new ResponseEntity<>(new Company(), HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/v2/companies/{id}")
+    public ResponseEntity<CompanyVersionTwo> getCompaniesVersionTwo(@PathVariable("id") String id) {
+
+        if(companiesVersionTwoMap.containsKey(id))
+            return ResponseEntity.ok(companiesVersionTwoMap.get(id));
+        return new ResponseEntity<>(new CompanyVersionTwo(), HttpStatus.NOT_FOUND);
+
     }
 }
